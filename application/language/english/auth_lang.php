4c8008a54379b565f2352993f727e8b1fc56b6d1
tataadin <tataa2237@gmail.com>
2026-08-12 20:52:50 +0700

    add lang

---
+++ application/language/english/auth_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang["login_title"] = "Sign in to start your session";
+$lang["login_username"] = "Nama Pengguna";
+$lang["login_password"] = "Kata Sandi";
+
+$lang["error_email_invalid"] = "Format email yang Anda masukkan salah.";
+$lang["error_password_weak"] = "Kata sandi terlalu lemah.";
